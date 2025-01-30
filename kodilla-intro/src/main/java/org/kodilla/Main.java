33885d4d33a1dde8c78b3dc6cafc2a36ea7e7eea
Norbert Ucieklak <n.ucieklak@gmail.com>
2025-01-30 21:56:20 +0100

    08 Variables

--- kodilla-intro/src/main/java/org/kodilla/Main.java
+++ kodilla-intro/src/main/java/org/kodilla/Main.java
@@ -1,17 +1,13 @@
 package org.kodilla;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(myName);
     }
 }
---
+++ kodilla-intro/src/main/java/org/kodilla/MainExtended.java
@@ -0,0 +1,26 @@
+package org.kodilla;
+
+public class MainExtended {
+    public static void main(String[] args) {
+
+        // my 6 variables
+
+        String firstName = "Norbert";
+        String lastName = "Ucieklak";
+        int myAge = 45;
+        float myHeight = 1.79f;
+        char myCarFirstLetter ='M';
+        boolean joinedKodilla = true;
+
+        // display my variables
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(myAge);
+        System.out.println(myHeight);
+        System.out.println(myCarFirstLetter);
+        System.out.println("Did I join Kodilla course: " + joinedKodilla);
+
+
+    }
+}
