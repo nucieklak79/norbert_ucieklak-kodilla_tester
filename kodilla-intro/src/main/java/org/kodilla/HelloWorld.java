7449f7ed7f776d6e4f14132e52606931da8c2bf2
Norbert Ucieklak <n.ucieklak@gmail.com>
2025-01-30 21:32:41 +0100

    07 Data types

---
+++ kodilla-intro/src/main/java/org/kodilla/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.kodilla;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Norbert");
+        System.out.println("45 lat");
+        System.out.println("U");
+    }
+}
