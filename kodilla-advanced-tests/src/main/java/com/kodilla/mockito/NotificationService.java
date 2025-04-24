16f9bc0a08a25bdd5875af236e4435bbe73c8f39
Norbert Ucieklak <n.ucieklak@gmail.com>
2025-04-24 06:02:52 +0200

    7.6 Mockito

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/NotificationService.java
@@ -0,0 +1,15 @@
+package com.kodilla.mockito;
+
+import com.kodilla.notification.Client;
+import com.kodilla.notification.Notification;
+
+public class NotificationService {
+
+    public void addSubscriber(Client client) {
+
+    }
+
+    public void sendNotification(Notification notification) {
+
+    }
+}
