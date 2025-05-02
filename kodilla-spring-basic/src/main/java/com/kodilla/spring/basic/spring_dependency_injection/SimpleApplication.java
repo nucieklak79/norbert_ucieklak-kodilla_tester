a98179d1f13f7dcb91960ad1a8bb146da8444acc
Norbert Ucieklak <n.ucieklak@gmail.com>
2025-05-02 14:11:49 +0200

    9.3 Spring interfaces

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/SimpleApplication.java
@@ -1,12 +1,15 @@
 package com.kodilla.spring.basic.spring_dependency_injection;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SimpleApplication {
     @Autowired
-    private SkypeMessageService messageService;
+    @Qualifier(value = "facebookMessageService")
+    private MessageService messageService;
 
     public String processMessage(String message, String receiver) {
         if (checkReceiver(receiver)) {
